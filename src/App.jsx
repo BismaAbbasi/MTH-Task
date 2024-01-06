a65f5ff66a2a497c89218377aa2390846db0264a
BismaAbbasi <104329592+BismaAbbasi@users.noreply.github.com>
2024-01-06 14:31:13 -0800

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Bisma Abbasi",
+  title: "Front-end developer",
+  email: "BismaAbbasi789@gmail.com.com",
+  gitHub: "BismaAbbasi",
+  instagram: "bisma.abbasiii",
+  linkedIn: "bisma-abbasi",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
